fa960e3f7903c12486a17f597eed3f7430527d80
mdsajalali <itzsajal.me@gmail.com>
2023-06-29 21:44:49 +0600

    one cart remove

--- js/product.js
+++ js/product.js
@@ -2,7 +2,6 @@ const products = [
   { id: 1, name: "Baby Baggy Pants", price: 20 },
   { id: 2, name: "Black T-Shirt", price: 10 },
   { id: 3, name: "Blue Leather Sandal", price: 15 },
-  { id: 3, name: "Blue Leather Shirt", price: 45 },
 ];
 
 export default products;
